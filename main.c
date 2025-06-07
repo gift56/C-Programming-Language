988baf7277796bcd470f4e0e5d1985f52a7e5030
gift56 <efeasiughu@gmail.com>
2025-06-07 13:58:47 -0500

    working on if else statement

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    // COMPOUND INTEREST CALCULATOR
-    double principle = 0.0;
-    double rate = 0.0;
-    int years = 0;
-    int timesCompounded = 0;
-    double total = 0.0;
+    // IF ELSE CONDITION
+    int age = 0;
 
-    printf("Compound Interest Calculator\n");
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-    printf("Enter the principal (P): ");
-    scanf("%lf", &principle);
-
-    printf("Enter the interest rate % (r): ");
-    scanf("%lf", &rate);
-    rate = rate / 100;
-
-    printf("Enter the # of years (t): ");
-    scanf("%d", &years);
-
-    printf("Enter # of times compounded per year (n): ");
-    scanf("%d", &timesCompounded);
-
-    total = principle * pow(1 + rate / timesCompounded, timesCompounded * years);
-
-    printf("After %d years, the total will be $%.2lf", years, total);
+    if (age >= 65)
+    {
+        printf("You're a senoir");
+    }
+    else if (age >= 18)
+    {
+        printf("You're a adult");
+    }
+    else if (age < 0)
+    {
+        printf("You're haven't been born yet");
+    }
+    else if (age == 0)
+    {
+        printf("You are a newborn");
+    }
+    else
+    {
+        printf("You are a child");
+    }
 
     return 0;
 }
