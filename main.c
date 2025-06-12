232dc2f4a7e8d27c661e7d175ef6649ee32604ae
gift56 <efeasiughu@gmail.com>
2025-06-12 10:39:28 -0500

    worked on return in functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void happyBirthday(char name[], int age)
 {
@@ -10,8 +11,34 @@ void happyBirthday(char name[], int age)
     printf("\nYou are %d years old!\n", age);
 }
 
+// FUNCTION WITH THE RETURN VALUE
+
+int square(int num)
+{
+    return num * num;
+}
+
+bool ageCheck(int age)
+{
+    if (age >= 18)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
 int main()
 {
+
+    int x = square(2);
+    int y = square(4);
+    int z = square(6);
+
+    printf("Value x is: %d\n", x);
+    printf("Value y is: %d\n", y);
+    printf("Value z is: %d\n", z);
+
     char name[50] = "";
     int age = 0;
 
@@ -23,5 +50,6 @@ int main()
     scanf("%d", &age);
 
     happyBirthday(name, age);
+
     return 0;
 }
