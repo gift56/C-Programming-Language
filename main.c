aaffc9f75239e86e15c7bff50d75c125de9cd35d
gift56 <efeasiughu@gmail.com>
2025-06-12 14:07:37 -0500

    worked on  file feature

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 int main()
 {
 
-    // Write A File
-
-    FILE *pFile = fopen("output.txt", "w");
-
-    char text[] = "I love coding in C!\n";
+    // Read A File
+    FILE *pFile = fopen("input.txt", "r");
+    char buffer[1024] = {0};
 
     if (pFile == NULL)
     {
         printf("Error opening file\n");
         return 1;
     }
-    
-    fprintf(pFile, "%s", text);
-    printf("File was written successfully\n");
+
+    while (fgets(buffer, sizeof(buffer), pFile) != NULL)
+    {
+        printf("%s", buffer);
+    }
+
+    printf("File was Read successfully\n");
 
     fclose(pFile);
 
